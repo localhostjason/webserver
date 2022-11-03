72c23fe6c9f2637eb6b3d9346af6d8892106b9cc
zhangjie <zhangjie@tcxa.com.cn>
2022-11-03 10:39:49 +0800

    change test_main_server

--- test_main_server.go
+++ test_main_server.go
@@ -22,9 +22,7 @@ func NewTMainServer(configPath string, setMainWorkFunc MainWorkFunc) *TMainServe
 // Run 可根据自己业务 替换扩展
 func (m *TMainServer) Run() {
 	configPath := flag.String("p", m.DefaultConfigPath, "path to config")
-	initDB := flag.Bool("i", false, "int db")
 	testCmd := flag.Bool("t", false, "test cmd")
-	dumpConfig := flag.Bool("d", false, "dump default config")
 
 	// for service
 	singleMode := flag.Bool("x", false, "start, no daemon/service mode")
@@ -34,23 +32,6 @@ func (m *TMainServer) Run() {
 
 	if err := config.SetConfigFile(*configPath); err != nil {
 		fmt.Println("failed to set config path", *configPath, err)
-		return
-	}
-
-	// commands
-
-	if *dumpConfig {
-		daemonx.DumpDefaultConfig()
-		return
-	}
-
-	// DB 初始表结构和默认值
-	if *initDB {
-		if err := daemonx.SyncDB(); err != nil {
-			fmt.Println("error when sync db schema", err)
-			return
-		}
-		fmt.Println("success: sync db schema")
 		return
 	}
 
