07591bf8ed78bdbb585a35d76d5010bcfaedc554
zhangjie <zhangjie@tcxa.com.cn>
2023-03-23 13:16:50 +0800

    fix: 修改路径 兼容 run / build

--- server/path.go
+++ server/path.go
@@ -2,25 +2,50 @@ package server
 
 import (
 	"os"
+	"path"
 	"path/filepath"
+	"runtime"
+	"strings"
 )
 
-func getWorkDir() (string, error) {
-	// 工作目录 类型 pwd
-	exeDir, err := os.Getwd()
-	return exeDir, err
-}
-
 func GetExeDir() (string, error) {
-	// 程序目录， exe 二进制文件路径
-	ex, err := os.Executable()
+	dir, err := getCurrentAbPathByExecutable()
 	if err != nil {
 		return "", err
 	}
-	exPath := filepath.Dir(ex)
 
-	if os.Getenv("DEBUG") != "" {
-		return getWorkDir()
+	if strings.Contains(dir, getTmpDir()) {
+		return getCurrentAbPathByCaller(), nil
 	}
-	return exPath, nil
+	return dir, nil
+}
+
+// 获取系统临时目录，兼容go run
+func getTmpDir() string {
+	dir := os.Getenv("TEMP")
+	if dir == "" {
+		dir = os.Getenv("TMP")
+	}
+	res, _ := filepath.EvalSymlinks(dir)
+	return res
+}
+
+// 获取当前执行文件绝对路径
+func getCurrentAbPathByExecutable() (string, error) {
+	exePath, err := os.Executable()
+	if err != nil {
+		return "", err
+	}
+	res, err := filepath.EvalSymlinks(filepath.Dir(exePath))
+	return res, err
+}
+
+// 获取当前执行文件绝对路径（go run）
+func getCurrentAbPathByCaller() string {
+	var abPath string
+	_, filename, _, ok := runtime.Caller(0)
+	if ok {
+		abPath = path.Dir(filename)
+	}
+	return abPath
 }
