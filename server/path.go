459ea80fe3f0b4ce002185784bb54db7502b9858
zhangjie <zhangjie@tcxa.com.cn>
2023-03-23 13:35:02 +0800

    fix: 修改路径 兼容 run / build

--- server/path.go
+++ server/path.go
@@ -2,50 +2,28 @@ package server
 
 import (
 	"os"
-	"path"
 	"path/filepath"
-	"runtime"
 	"strings"
 )
 
 func GetExeDir() (string, error) {
-	dir, err := getCurrentAbPathByExecutable()
+	exePath, err := os.Executable()
 	if err != nil {
 		return "", err
 	}
+	res := filepath.Dir(exePath)
 
-	if strings.Contains(dir, getTmpDir()) {
-		return getCurrentAbPathByCaller(), nil
+	if strings.Contains(exePath, getTmpDir()) {
+		// run 模式下，确在当前程序入口目录
+		return os.Getwd()
 	}
-	return dir, nil
+	return res, nil
 }
 
-// 获取系统临时目录，兼容go run
 func getTmpDir() string {
 	dir := os.Getenv("TEMP")
 	if dir == "" {
 		dir = os.Getenv("TMP")
 	}
-	res, _ := filepath.EvalSymlinks(dir)
-	return res
-}
-
-// 获取当前执行文件绝对路径
-func getCurrentAbPathByExecutable() (string, error) {
-	exePath, err := os.Executable()
-	if err != nil {
-		return "", err
-	}
-	res, err := filepath.EvalSymlinks(filepath.Dir(exePath))
-	return res, err
-}
-
-// 获取当前执行文件绝对路径（go run）
-func getCurrentAbPathByCaller() string {
-	var abPath string
-	_, filename, _, ok := runtime.Caller(0)
-	if ok {
-		abPath = path.Dir(filename)
-	}
-	return abPath
+	return dir
 }
